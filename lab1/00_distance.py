319aaec5cb50ba5533af28ec661037f743414018
Niktokton <151038902+Niktokton@users.noreply.github.com>
2025-02-06 01:16:12 +0500

    Update 00_distance.py

--- lab1/00_distance.py
+++ lab1/00_distance.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Есть словарь координат городов
+from math import sqrt
 
 sites = {
     'Moscow': (550, 370),
@@ -9,12 +10,23 @@ sites = {
     'Paris': (480, 480),
 }
 
+def calculate_distance(city1, city2):
+    x1, y1 = sites[city1]
+    x2, y2 = sites[city2]
+    return sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
 distances = {}
 
-# TODO здесь заполнение словаря
+for city1 in sites.keys():
+    distances[city1] = {}
+    for city2 in sites.keys():
+        if city1 != city2:
+            distance = calculate_distance(city1, city2)
+            distances[city1][city2] = round(distance, 2)
 
 print(distances)
 
