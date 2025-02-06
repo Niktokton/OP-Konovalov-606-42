8074a743a7771e349cf7b2a03e5df99963580fc1
Niktokton <151038902+Niktokton@users.noreply.github.com>
2025-02-06 14:18:02 +0500

    Update 07_secret.py

--- lab1/07_secret.py
+++ lab1/07_secret.py
@@ -33,3 +33,8 @@ secret_message = [
 #   Если нужны вычисления и разные пробы - делайте это в консоли пайтона, тут нужен только результат
 
 # TODO вывести расшифрованное сообщение
+print(secret_message[0][3], end=' ')
+print(secret_message[1][9:13], end=' ')
+print(secret_message[2][5:15:2], end=' ')
+print(secret_message[3][12:6:-1], end=' ')
+print(secret_message[4][20:15:-1])
