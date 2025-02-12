8797e1e15153bd04f52dc5fc1e28d2ce5de93682
Niktokton <151038902+Niktokton@users.noreply.github.com>
2025-02-12 22:12:13 +0500

    Update and rename 02_operations.py to _02_operations.py

---
+++ lab1/_02_operations.py
@@ -0,0 +1,15 @@
+def answer1():
+    result = (1 + 2) * 3
+    print(result)
+
+
+result = ((1 + 2) * 3 - 4) * 5
+
+
+def answer2():
+    print(result)
+
+
+if __name__ == "__main__":
+    answer1()
+    answer2()
