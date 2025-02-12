b9089ee4ccc4d40ed6eb5b7523025d14fbc0b927
Niktokton <151038902+Niktokton@users.noreply.github.com>
2025-02-12 22:46:10 +0500

    Update and rename 03_favorite_movies.py to _03_favorite_movies.py

---
+++ lab1/_03_favorite_movies.py
@@ -0,0 +1,12 @@
+my_favorite_movies = 'Терминатор, Пятый элемент, Аватар, Чужие, Назад в будущее'
+
+
+def answer():
+    print(my_favorite_movies[0:10])
+    print(my_favorite_movies[-15:])
+    print(my_favorite_movies[12:25])
+    print(my_favorite_movies[-22:-17])
+
+
+if __name__ == "__main__":
+    answer()
