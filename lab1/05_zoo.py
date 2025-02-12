d5ecd496fcf146963f6b4f990905ed084dc1bc7e
Niktokton <151038902+Niktokton@users.noreply.github.com>
2025-02-12 22:56:41 +0500

    Update 05_zoo.py

--- lab1/05_zoo.py
+++ lab1/05_zoo.py
@@ -1,31 +1,17 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-# есть список животных в зоопарке
-
 zoo = ['lion', 'kangaroo', 'elephant', 'monkey', ]
 
-# посадите медведя (bear) между львом и кенгуру
-#  и выведите список на консоль
-# TODO здесь ваш код
-zoo.insert(2, 'bear')
-print(zoo)
 
-# добавьте птиц из списка birds в последние клетки зоопарка
-birds = ['rooster', 'ostrich', 'lark', ]
-#  и выведите список на консоль
-# TODO здесь ваш код
-zoo += birds
-print(zoo)
+def answer(zoo):
+    zoo.insert(2, 'bear')
+    print(zoo)
+    birds = ['rooster', 'ostrich', 'lark', ]
+    zoo += birds
+    print(zoo)
+    zoo.remove('elephant')
+    print(zoo)
+    print("Лев - ", zoo.index('lion') + 1)
+    print("Жаворонок - ", zoo.index('lark') + 1)
 
-# уберите слона
-#  и выведите список на консоль
-# TODO здесь ваш код
-zoo.remove('elephant')
-print(zoo)
 
-# выведите на консоль в какой клетке сидит лев (lion) и жаворонок (lark).
-# Номера при выводе должны быть понятны простому человеку, не программисту.
-# TODO здесь ваш код
-print("Лев - ", zoo.index('lion') + 1)
-print("Жаворонок - ", zoo.index('lark') + 1)
+if __name__ == "__main__":
+    answer(zoo)
