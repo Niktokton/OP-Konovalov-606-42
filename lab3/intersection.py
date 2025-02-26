b6787b94af5167e580e98f6c35f206e9a553a593
Niktokton <151038902+Niktokton@users.noreply.github.com>
2025-02-26 22:21:44 +0500

    Create intersection.py

---
+++ lab3/intersection.py
@@ -0,0 +1,17 @@
+# def intersect(list1, list2):  # Без рекурсии
+#     return list(set(list1) & set(list2))
+
+
+def intersect(list1, list2):  # Рекурсия
+    if not list1 or not list2:
+        return []
+    tmp = list1[0]
+    if tmp in list2:
+        return [tmp] + intersect(list1[1:], list2)
+    else:
+        return intersect(list1[1:], list2)
+
+
+print(intersect([1, 2, 3, 4], [2, 3, 4, 6, 8]))
+print(intersect([5, 8, 2], [2, 9, 1]))
+print(intersect([5, 8, 2], [7, 4]))
