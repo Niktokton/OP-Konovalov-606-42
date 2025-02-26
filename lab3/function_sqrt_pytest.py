5addf8be1f3381f8c3638eef26f9f2b33e82a2fc
Niktokton <151038902+Niktokton@users.noreply.github.com>
2025-02-26 23:48:51 +0500

    Create function_sqrt_pytest.py

---
+++ lab3/function_sqrt_pytest.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+def test_function_sqrt():
+    assert function_sqrt(1) == 1.7320508075688772
+    assert function_sqrt(2) == 2.1753277471610746
+    assert function_sqrt(10) == 2.3027750032160834
+
+
+test_function_sqrt()
